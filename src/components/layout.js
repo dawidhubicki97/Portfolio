a5b2d1e4b5ddbe5d947aa56ee18f35cf0a9827af
Dawid <daw8hub@gmail.com>
2021-03-16 20:48:27 +0100

    Social Links added

--- src/components/layout.js
+++ src/components/layout.js
@@ -8,6 +8,7 @@ import "./layout.css"
 import Navbar from "./Navbar"
 import About from "./about"
 import Home from "./home"
+import Person from "./person"
 
 const Layout = (homeImageData, { children }) => {
   return (
@@ -15,11 +16,14 @@ const Layout = (homeImageData, { children }) => {
       <div className="header">
         <Navbar></Navbar>
         <Img
+          style={{ height: "100%", width: "100vw" }}
+          imgStyle={{ objectFit: "fill" }}
           fluid={
             homeImageData.homeImageData.backgroundImage.childImageSharp.fluid
           }
           alt=""
         />
+        <Person></Person>
         <Home></Home>
       </div>
       <About></About>
---
+++ src/components/person.js
@@ -0,0 +1,14 @@
+import React from "react"
+import SocialLinks from "../constants/socialLinks"
+
+export default function Person() {
+  return (
+    <div class="person">
+      <article>
+        <h1>Dawid Hubicki </h1>
+        <h4>Front-End Developer</h4>
+        <SocialLinks></SocialLinks>
+      </article>
+    </div>
+  )
+}
