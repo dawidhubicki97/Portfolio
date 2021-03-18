02664bf6b8e2e73ca8c57dbc4c8a8de47665c0aa
Dawid <daw8hub@gmail.com>
2021-03-18 19:28:01 +0100

    About css modified

--- src/components/about.js
+++ src/components/about.js
@@ -1,11 +1,43 @@
 import React from "react"
+import { FaCode, FaGraduationCap, FaAndroid } from "react-icons/fa"
 
 export default function About() {
   return (
     <div className="about">
-      <div className="about-box">Raz</div>
-      <div className="about-box">Dwa</div>
-      <div className="about-box">Trzy</div>
+      <h1>O mnie</h1>
+      <div className="about-underline"></div>
+      <div className="about-container">
+        <div className="about-box">
+          <FaCode size={40}></FaCode>
+          <h4>Web Developer</h4>
+          <div className="about-underline"></div>
+          <p>
+            Tekst dlugiTekst dlugiTekst dlugiTekst dlugiTekst dlugiTekst
+            dlugiTekst dlugiTekst dlugiTekst dlugiTekst dlugiTekst dlugiTekst
+            dlugi
+          </p>
+        </div>
+        <div className="about-box">
+          <FaAndroid size={40}></FaAndroid>
+          <h4>Android Developer</h4>
+          <div className="about-underline"></div>
+          <p>
+            Tekst dlugiTekst dlugiTekst dlugiTekst dlugiTekst dlugiTekst
+            dlugiTekst dlugiTekst dlugiTekst dlugiTekst dlugiTekst dlugiTekst
+            dlugi
+          </p>
+        </div>
+        <div className="about-box">
+          <FaGraduationCap size={40}></FaGraduationCap>
+          <h4>Student</h4>
+          <div className="about-underline"></div>
+          <p>
+            Tekst dlugiTekst dlugiTekst dlugiTekst dlugiTekst dlugiTekst
+            dlugiTekst dlugiTekst dlugiTekst dlugiTekst dlugiTekst dlugiTekst
+            dlugi
+          </p>
+        </div>
+      </div>
     </div>
   )
 }
