dd2f2362de2815374de757413fcfc08c3e05bdbf
Dawid <daw8hub@gmail.com>
2021-03-28 16:54:54 +0200

    colors changed

--- src/constants/socialLinks.js
+++ src/constants/socialLinks.js
@@ -1,11 +1,5 @@
 import React from "react"
-import {
-  FaFacebookSquare,
-  FaLinkedin,
-  FaDribbbleSquare,
-  FaBehanceSquare,
-  FaTwitterSquare,
-} from "react-icons/fa"
+import { FaFacebookSquare, FaLinkedin, FaGithub } from "react-icons/fa"
 
 const data = [
   {
@@ -20,17 +14,7 @@ const data = [
   },
   {
     id: 3,
-    icon: <FaDribbbleSquare className="social-icon"></FaDribbbleSquare>,
-    url: "https://www.twitter.com",
-  },
-  {
-    id: 4,
-    icon: <FaBehanceSquare className="social-icon"></FaBehanceSquare>,
-    url: "https://www.twitter.com",
-  },
-  {
-    id: 5,
-    icon: <FaTwitterSquare className="social-icon"></FaTwitterSquare>,
+    icon: <FaGithub className="social-icon"></FaGithub>,
     url: "https://www.twitter.com",
   },
 ]
