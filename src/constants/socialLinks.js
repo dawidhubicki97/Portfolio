0d47454b4d9b2b8b318301c94fb67a3978888b7f
Dawid <daw8hub@gmail.com>
2021-04-13 17:19:18 +0200

    Navbar Menu css changes

--- src/constants/socialLinks.js
+++ src/constants/socialLinks.js
@@ -5,17 +5,17 @@ const data = [
   {
     id: 1,
     icon: <FaFacebookSquare className="social-icon"></FaFacebookSquare>,
-    url: "https://www.twitter.com",
+    url: "https://www.facebook.com/dawid.hubicki.5/",
   },
   {
     id: 2,
     icon: <FaLinkedin className="social-icon"></FaLinkedin>,
-    url: "https://www.twitter.com",
+    url: "https://www.linkedin.com/in/dawid-hubicki-2b5833202/",
   },
   {
     id: 3,
     icon: <FaGithub className="social-icon"></FaGithub>,
-    url: "https://www.twitter.com",
+    url: "https://github.com/dawidhubicki97",
   },
 ]
 const links = data.map(link => {
