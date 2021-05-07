068f86a1e954bf75ed78076e983cc773824687e5
Dawid <daw8hub@gmail.com>
2021-05-07 19:46:03 +0200

    Deploy Bug Fixed

--- gatsby-config.js
+++ gatsby-config.js
@@ -29,21 +29,26 @@ module.exports = {
         icon: `src/images/computer_icon.png`, // This path is relative to the root of the site.
       },
     },
-    `gatsby-plugin-gatsby-cloud`,
     {
       resolve: `gatsby-transformer-remark`,
       options: {
         plugins: [
-          "gatsby-remark-relative-images",
+          // gatsby-remark-relative-images must
+          // go before gatsby-remark-images
+          {
+            resolve: `gatsby-remark-relative-images`,
+          },
           {
             resolve: `gatsby-remark-images`,
             options: {
+              // It's important to specify the maxWidth (in pixels) of
+              // the content container as this plugin uses this as the
+              // base for generating different widths of each image.
               maxWidth: 590,
             },
           },
         ],
       },
-    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
