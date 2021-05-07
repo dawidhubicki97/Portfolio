fac3e283c121f735bdebe714ed60b57874f060e0
Dawid <daw8hub@gmail.com>
2021-05-07 13:11:12 +0200

    Content Added

--- src/components/about.js
+++ src/components/about.js
@@ -4,38 +4,42 @@ import { FaCode, FaGraduationCap, FaAndroid } from "react-icons/fa"
 export default function About() {
   return (
     <div className="about" id="about">
-      <h1>O mnie</h1>
+      <div className="about-header">
+        <h1>O mnie</h1>
+      </div>
       <div className="about-underline"></div>
       <div className="about-container">
         <div className="about-box">
-          <FaCode size={40}></FaCode>
-          <h4>Web Developer</h4>
-          <div className="about-underline"></div>
-          <p>
-            Tekst dlugiTekst dlugiTekst dlugiTekst dlugiTekst dlugiTekst
-            dlugiTekst dlugiTekst dlugiTekst dlugiTekst dlugiTekst dlugiTekst
-            dlugi
-          </p>
+          <div className="about-boxHeader">
+            <FaCode size={40}></FaCode>
+            <h4>Web Developer</h4>
+            <div className="about-underline"></div>
+          </div>
+          Moim głównym zainteresowaniem są aplikacje internetowe. Zajmuje się
+          ich tworzeniem oraz projektowaniem. Technologie które zazwyczaj
+          wykorzystuje to
+          <b> html & css, javascript, react, gatsby, git, graphql, firebase</b>.
         </div>
         <div className="about-box">
-          <FaAndroid size={40}></FaAndroid>
-          <h4>Android Developer</h4>
-          <div className="about-underline"></div>
-          <p>
-            Tekst dlugiTekst dlugiTekst dlugiTekst dlugiTekst dlugiTekst
-            dlugiTekst dlugiTekst dlugiTekst dlugiTekst dlugiTekst dlugiTekst
-            dlugi
-          </p>
+          <div className="about-boxHeader">
+            <FaAndroid size={40}></FaAndroid>
+
+            <h4>Android Developer</h4>
+            <div className="about-underline"></div>
+          </div>
+          Uczę się również tworzenia aplikacji mobilnych na system Android.
+          Technologię które zazwyczaj wykorzstuje to:
+          <b> Kotlin, Java, Firebase.</b>
         </div>
         <div className="about-box">
-          <FaGraduationCap size={40}></FaGraduationCap>
-          <h4>Student</h4>
-          <div className="about-underline"></div>
-          <p>
-            Tekst dlugiTekst dlugiTekst dlugiTekst dlugiTekst dlugiTekst
-            dlugiTekst dlugiTekst dlugiTekst dlugiTekst dlugiTekst dlugiTekst
-            dlugi
-          </p>
+          <div className="about-boxHeader">
+            <FaGraduationCap size={40}></FaGraduationCap>
+            <h4>Student</h4>
+            <div className="about-underline"></div>
+          </div>
+          Jestem studentem na studiach magisterskich na kierunku Informatyka.
+          Uzyskałem stopień inżyniera na kierunku informatyka na Uniwersytecie
+          Rzeszowskim (specjalizacja aplikacje internetowe).
         </div>
       </div>
     </div>
--- src/components/home.js
+++ src/components/home.js
@@ -6,20 +6,22 @@ export default function Home() {
     <div className="home" id="home">
       <article>
         <div className="home-title">Dawid Hubicki</div>
-        <Typewriter
-          options={{
-            loop: true,
-          }}
-          onInit={typewriter => {
-            typewriter
-              .typeString("Front-End Developer")
-              .pauseFor(2000)
-              .deleteAll()
-              .typeString("Web Designer")
-              .pauseFor(2000)
-              .start()
-          }}
-        ></Typewriter>
+        <div className="home-typewriter">
+          <Typewriter
+            options={{
+              loop: true,
+            }}
+            onInit={typewriter => {
+              typewriter
+                .typeString("Front-End Developer")
+                .pauseFor(2000)
+                .deleteAll()
+                .typeString("Web Designer")
+                .pauseFor(2000)
+                .start()
+            }}
+          ></Typewriter>
+        </div>
         <SocialLinks></SocialLinks>
       </article>
     </div>
